62197b348bf15da77193d49e8069ac35b1c95516
Jexzas <jdrmail2007@yahoo.com>
2026-02-28 01:11:49 -0600

    WOW I SURE DID ADD SOME STUFF

--- boardAmbiance.gd
+++ boardAmbiance.gd
@@ -5,8 +5,15 @@ var reversed: bool = false
 var points: Array
 var stars: TextureRect
 var currentSize: Vector2 
+var positions: Dictionary = {}
+var piecesReady: int = 0
+var sound = preload("res://sound/clump.ogg")
+var bing: SceneTreeTimer 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for child in get_tree().get_nodes_in_group("markers"):
+			positions[child.get_name()] = child.get_position()
+	print(positions)
 	board = %BoardNode
 	stars = %Background
 	currentSize = stars.get_minimum_size()
@@ -15,8 +22,22 @@ func _ready() -> void:
 		if i is not Marker2D:
 			points.erase(i)
 
+func readyThePieces(delta:float):
+	for piece in get_tree().get_nodes_in_group("pieces"):
+		while piece.position != piece.defPos.position:
+			piece.position = piece.position.lerp(piece.defPos.position, .7)
+			if !%AudioStreamPlayer2D.is_playing():
+				%AudioStreamPlayer2D.stream = sound
+				%AudioStreamPlayer2D.play()
+		piecesReady += 1
+		bing = get_tree().create_timer(.3)
+		await bing.timeout
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if piecesReady <= 15:
+		readyThePieces(delta)
+		
 	currentSize = Vector2(currentSize.x + .003, currentSize.y + .003)
 	stars.custom_minimum_size = currentSize
 	if timer >= 3:
--- pieceBeh.gd
+++ pieceBeh.gd
@@ -1,21 +1,30 @@
 extends Node
 @export var piece: Node2D
+@export var defPos: Marker2D
 var timer: float
 var pScale: Vector2
+var rand = RandomNumberGenerator.new()
+var deci: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer = 0
+	deci = 10
 	pScale = piece.transform.get_scale()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	timer += delta
-	var evenGuy: int = round(timer)
-	if evenGuy % 10 == 0:
-		animate(piece)
+	if timer > deci:
+		animate(delta)
+		deci += 10
 
 
-func animate(node: Node2D) -> void:
-	for i in range(900):
-		piece.transform.scaled(Vector2(i, i))
-		
+func animate(delta: float) -> void:
+	print("something")
+	var base = pScale
+	for i in range(100):
+		var my_random_number = rand.randf_range(.995, 1.005)
+		piece.apply_scale(Vector2(my_random_number, my_random_number))
+		await get_tree().create_timer(.0002 * delta).timeout
+		print(piece.transform.get_scale())
+	piece.scale = pScale
